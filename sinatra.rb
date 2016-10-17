146c00f3675f7858b8a9080758ca5ef28d49cc39
znikeriz <nerijus.jonaitis5@gmail.com>
2016-10-17 20:41:52 +0300

    'all_file'

--- sinatra.rb
+++ sinatra.rb
@@ -9,35 +9,45 @@ $pg = PGconn.connect("localhost", 5432, '', '', "new_customer", "postgres", "123
 
 class ManoApp < Sinatra::Base
 
+  get '/' do
+    @per_page = 10
+    @page = params['page'].to_i
+    @customers = Customer.all(@per_page, @page)
+    @last_page = Customer.count / @per_page
+    erb :main
+  end
 
-get '/' do
-	@per_page = 10
-	@page = params['page'].to_i
-	binding.pry
-	@customers = Customer.all(@per_page, @page)
-	@last_page = Customer.count / @per_page
-	erb :main
-end
+  get '/new_customer' do
+    @customer = Customer.new(name: '', surname: '', age: nil, code: nil, id: nil)
+    erb :new_customer
+  end
 
-get '/customers/:id' do
-    @customer = Customer.find(params[:id])
+  post '/new_customers' do 
+    @customer = Customer.new(name: params[:name], surname: params[:surname],
+                             age: params[:age], code: params[:code], id: nil)
+    @customer.create
     erb :show_customer
   end
 
-get '/customers/:id/edit' do
-	@customer = Customer.find(params[:id])
-	erb :edit_customer
-end
+  get '/customers/:id' do
+      @customer = Customer.find(params[:id])
+      erb :show_customer
+    end
 
-post '/customers' do
-	@customer = Customer.find(params[:id])
-	@customer.update(params)
-	redirect "customers/#{params[:id]}"
-end
+  get '/customers/:id/edit' do
+    @customer = Customer.find(params[:id])
+    erb :edit_customer
+  end
 
-def pg
-  $pg = PGconn.connect("localhost", 5432, '', '', "new_customer", "postgres", "123456")
-end
+  post '/customers' do
+    @customer = Customer.find(params[:id])
+    @customer.update(params)
+    redirect "customers/#{params[:id]}"
+  end
+
+  def pg
+    $pg = PGconn.connect("localhost", 5432, '', '', "new_customer", "postgres", "123456")
+  end
 
 end
 
